338e6dc3a9ce57bbff72db1d8f67b4e840ccc618
Jeremy Magland <jeremy.magland@gmail.com>
2017-01-17 09:49:44 -0500

    remove fortran examples

--- examples/example4_fortran_complex/test_norm_it.m
+++
@@ -1,7 +0,0 @@
-
-fprintf('\n\n*** Be sure to add the mcwrap directory to your path ***\n\n');
-fprintf('\n\n*** You must also install a MATLAB-compatible Fortran compiler ***\n\n');
-
-mcwrap('norm_it.mcwrap');
-
-norm_it([1,1+i,1-i,5i,-3i,3+4i])
